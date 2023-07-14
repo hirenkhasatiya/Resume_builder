30174a93ee971b1a4fe1796339bd86beb533e48c
hirenkhasatiya <khasatiyahiren43@gmail.com>
2023-07-14 11:48:18 +0530

    security set / clean

--- resume_app/lib/views/screen/Contact_info.dart
+++ resume_app/lib/views/screen/Contact_info.dart
@@ -9,11 +9,22 @@ class ContactInfo extends StatefulWidget {
 }
 
 class _ContactInfoState extends State<ContactInfo> {
+
   int currentIndex=0;
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController numberController = TextEditingController();
+  TextEditingController PasswordController = TextEditingController();
+
+  bool obscure = true;
+
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
         appBar: AppBar(
             leading: myBackButton(),
             title: Text("Contact Info"),
@@ -24,50 +35,65 @@ class _ContactInfoState extends State<ContactInfo> {
       body: Column(
         children: [
           SizedBox(
-            child: Row(
-              children: [
-                Expanded(
-                  child: InkWell(
-
-                    child: Container(
-                      height: 70,
-                      color: Colors.black,
-                      child: Center(
-                        child: Text(
-                          "Contact",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(50),
+                      onTap: (){
+                        setState(() {
+                          currentIndex=0;
+                        });
+                      },
+                      child: Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: currentIndex==0 ? Colors.green : Colors.black,
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Contact",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: InkWell(
-                    onTap: (){
-                      currentIndex = 1;
-                      BoxDecoration(
-                        border: currentIndex==1 ? Border(bottom: BorderSide(color: Colors.yellow,width: 10)):null,
-                      );
-                    },
-                    child: Container(
-                      height: 70,
-                      color: Colors.black,
-                      child: Center(
-                        child: Text(
-                          "Photo",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
+                  SizedBox(width: 10,),
+                  Expanded(
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(50),
+                      onTap: (){
+                        setState(() {
+                          currentIndex=1;
+                        });
+                      },
+                      child: Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: currentIndex==1 ? Colors.green : Colors.black,
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Photo",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           Padding(
@@ -77,16 +103,192 @@ class _ContactInfoState extends State<ContactInfo> {
               children: [
                 //contact
                 Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(20),
+                      topLeft: Radius.circular(20),
+                    ),
+                  ),
                   width: double.infinity,
                   padding: EdgeInsets.all(5),
-                  color: Colors.white,
                   child: Form(
+                    key: formKey,
                     child: Column(
                       children: [
-                        Text("Contact"),
-                        SizedBox(
-                          height: 10,
+                        TextFormField(
+                          validator: (val){
+                            if(val!.isEmpty)
+                              {
+                                return "Please Enter Name";
+                              }
+                            else
+                              {
+                                return null;
+                              }
+                          },
+                          controller: nameController,
+                          keyboardType: TextInputType.name,
+                          textInputAction: TextInputAction.next,
+                          decoration: InputDecoration(
+                            hintText: "Enter Name",
+                            labelText: "Name",
+                            prefixIcon: Icon(
+                              Icons.person,
+                              size: 25,
+                            ),
+                            suffixIcon: Padding(
+                              padding: EdgeInsets.only(right: 10),
+                              child: IconButton(
+                                onPressed: (){
+                                  setState(() {
+                                    nameController.clear();
+                                  });
+                                },
+                                icon: Icon(
+                                  Icons.clear
+                                ),
+                              ),
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50)
+                            )
+                          ),
                         ),
+                        SizedBox(height: 20,),
+                        TextFormField(
+                          validator: (val){
+                            if(val!.isEmpty)
+                              {
+                                return "Please Enter Email";
+                              }
+                            else
+                              {
+                                return null;
+                              }
+                          },
+                          controller: emailController,
+                          keyboardType: TextInputType.emailAddress,
+                          textInputAction: TextInputAction.next,
+                          decoration: InputDecoration(
+                            hintText: "Enter Email",
+                            labelText: "Email",
+                            prefixIcon: Icon(
+                              Icons.email_outlined,
+                              size: 25,
+                            ),
+                              suffixIcon: Padding(
+                                padding: EdgeInsets.only(right: 10),
+                                child: IconButton(
+                                  onPressed: (){
+                                    setState(() {
+                                      emailController.clear();
+                                    });
+                                  },
+                                  icon: Icon(
+                                      Icons.clear
+                                  ),
+                                ),
+                              ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50)
+                            )
+                          ),
+                        ),
+                        SizedBox(height: 20,),
+                        TextFormField(
+                          validator: (val){
+                            if(val!.isEmpty)
+                              {
+                                return "Please Enter your number";
+                              }
+                            else
+                              {
+                                return null;
+                              }
+                          },
+                          controller: numberController,
+                          keyboardType: TextInputType.number,
+                          textInputAction: TextInputAction.next,
+                          maxLength: 10,
+                          decoration: InputDecoration(
+                            hintText: "Enter number",
+                            labelText: "number",
+                            prefixIcon: Icon(
+                              Icons.phone,
+                              size: 25,
+                            ),
+                              suffixIcon: Padding(
+                                padding: EdgeInsets.only(right: 10),
+                                child: IconButton(
+                                  onPressed: (){
+                                    setState(() {
+                                      numberController.clear();
+                                    });
+                                  },
+                                  icon: Icon(
+                                      Icons.clear
+                                  ),
+                                ),
+                              ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50)
+                            )
+                          ),
+                        ),
+                        SizedBox(height: 20,),
+                        TextFormField(
+                          validator: (val){
+                            if(val!.isEmpty)
+                              {
+                                return "Please Enter your Password";
+                              }
+                            else
+                              {
+                                return null;
+                              }
+                          },
+                          controller: PasswordController,
+                          keyboardType: TextInputType.visiblePassword,
+                          textInputAction: TextInputAction.done,
+                          obscureText: obscure,
+                          decoration: InputDecoration(
+                            hintText: "Enter Your Password",
+                            labelText: "Password",
+                            prefixIcon: Icon(
+                              Icons.lock,
+                              size: 25,
+                            ),
+                              suffixIcon: Padding(
+                                padding: EdgeInsets.only(right: 10),
+                                child: IconButton(
+                                  onPressed: (){
+                                    setState(() {
+                                      obscure=!obscure;
+                                    });
+                                  },
+                                  icon: Icon(
+                                      obscure ? Icons.visibility : Icons.visibility_off,
+                                  ),
+                                ),
+                              ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50)
+                            )
+                          ),
+                        ),
+                        SizedBox(height: 20,),
+                        Row(
+                          children: [
+                            ElevatedButton(
+                                onPressed: (){
+                              setState(() {
+                                bool validated = formKey.currentState!.validate();
+                              });
+                            },
+                                child: const Text("Submit")
+                            ),
+                          ],
+                        )
                       ],
                     ),
                   ),
